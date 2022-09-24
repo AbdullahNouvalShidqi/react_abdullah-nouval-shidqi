291186d96a48101f8a34a7071121ea64310e1a9a
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-09-24 13:08:16 +0700

    praktikum done

---
+++ 16_React Form/praktikum/praktikum_react_form/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import Form from "../components/form/Form";
+import Header from "../components/header/Header";
+
+function Home() {
+    return(
+        <>
+            <Header />
+            <Form />
+        </>
+    );
+}
+
+export default Home;
